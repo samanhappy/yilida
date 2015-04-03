1929926b1c9b700389d40b09ce5bebe3ff7014b4
samanhappy <samanhappy@gmail.com>
2015-04-03 11:40:07 +0800

    first post

---
+++ data/product.js
@@ -0,0 +1,11 @@
+function ProductCtrl($scope){
+        $scope.products = [
+                {'name': 'A型帐篷布', 'img':'images/product_1.png','url':'/products/1.html'},
+                {'name': '250D高强牛津布', 'img':'images/product_2.png','url':'/products/2.html'},
+                {'name': '牛津布展示', 'img':'images/product_3.jpg','url':'/products/3.html'},
+                {'name': '牛津布展示', 'img':'images/product_4.jpg','url':'/products/4.html'},
+                {'name': '牛津布展示', 'img':'images/product_5.jpg','url':'/products/5.html'},
+                {'name': '牛津布展示', 'img':'images/product_6.jpg','url':'/products/6.html'},
+                {'name': '牛津布展示', 'img':'images/product_7.jpg','url':'/products/7.html'}
+        ]
+}
